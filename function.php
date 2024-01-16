56b3d90b610f18c1b3453f9c5f398a2d2f9f2cf1
Dudebaker <eristoffer@hotmail.com>
2024-01-16 13:37:15 +0100

    Update function.php

--- function.php
+++ function.php
@@ -4,24 +4,19 @@ function blockBadBots()
 {
   $robotsTxt = explode(PHP_EOL, file_get_contents('robots.txt'));
 
-  $disallowedRobots = [];
-
   foreach($robotsTxt as $line => $content)
   {
     $previousContent = str_replace('user-agent: ', 'user-agent:', strtolower($robotsTxt[$line - 1]));
 
-    if(str_replace(' ', '',  trim(strtolower($content))) == 'disallow:/' && strpos(strtolower($previousContent), 'user-agent:') !== false)
+    if(str_replace(' ', '',  trim(strtolower($content))) == 'disallow:/' && strpos(strtolower($previousContent), 'user-agent:') === false)
     {
-      $disallowedRobots[] = str_replace('user-agent:', '', $previousContent);
+      continue;
     }
-  }
 
-  foreach($disallowedRobots as $disallowedRobot)
-  {
-    if(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), $disallowedRobot) !== false)
+    if(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), str_replace('user-agent:', '', $previousContent)) !== false)
     {
       header("HTTP/1.0 403 FORBIDDEN");
       die();
-    }
+    }    
   }
 }
